e143788c0feab44ddc9445ce60160ef6c2f3acef
dianhsu <xudian.cn@gmail.com>
2021-04-08 19:24:42 +0800

    fix

--- stage2/softmax.h
+++ stage2/softmax.h
@@ -9,6 +9,7 @@ void softmaxForward(T (&input)[SEQ][DIM], T (&output)[SEQ][DIM]) {
 	T output_pl[SEQ][DIM];
 #pragma HLS ARRAY_PARTITION variable=output_pl dim=1 complete
 	SF_LOOP0: for (int i = 0; i < SEQ; ++i) {
+#pragma HLS PIPELINE
 		SF_LOOP1: for (int j = 0; j < DIM; ++j) {
 #pragma HLS UNROLL
 			input_pl[i][j] = input[i][j];
@@ -20,6 +21,7 @@ void softmaxForward(T (&input)[SEQ][DIM], T (&output)[SEQ][DIM]) {
 		tmp[j] = 0;
 	}
 	SF_LOOP3: for (int i = 0; i < SEQ; ++i) {
+#pragma HLS PIPELINE
 		SF_LOOP4: for (int j = 0; j < DIM; ++j) {
 #pragma HLS UNROLL
 			tmp[j] += input_pl[i][j];
@@ -30,6 +32,7 @@ void softmaxForward(T (&input)[SEQ][DIM], T (&output)[SEQ][DIM]) {
 		tmp[j] = 1/tmp[j];
 	}
 	SF_LOOP5: for (int i = 0; i < SEQ; ++i) {
+#pragma HLS PIPELINE
 		SF_LOOP6: for (int j = 0; j < DIM; ++j) {
 #pragma HLS UNROLL
 			output_pl[i][j] = input_pl[i][j] * tmp[j];
