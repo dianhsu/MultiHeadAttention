388c65108c49657b703128ab874203546fedf0c3
dianhsu <xudian.cn@gmail.com>
2021-04-08 09:48:36 +0800

    fix

--- default/run_hls.tcl
+++ default/run_hls.tcl
@@ -3,7 +3,7 @@
 ## Please DO NOT edit it.
 ## Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ############################################################
-open_project Multi_Head_Attention
+open_project hls
 set_top project_top
 add_files attention.h
 add_files dropout.h
@@ -12,7 +12,7 @@ add_files main.cpp
 add_files main.h
 add_files softmax.h
 add_files -tb main_tb.cpp
-open_solution "default" -flow_target vivado
+open_solution "solution" -flow_target vivado
 set_part {xcvu19p-fsva3824-2-e}
 create_clock -period 10 -name default
 csim_design
