90a5082d4a0ffefe3a197a6bbb2ae82f4f7870cf
dianhsu <xudian.cn@gmail.com>
2021-04-08 12:51:02 +0800

    fix

--- stage2/dropout.h
+++ stage2/dropout.h
@@ -3,7 +3,9 @@
 
 template<typename T, int DIM>
 void dropoutForward(T (&input)[DIM], T (&output)[DIM], T dropout_rate) {
+#pragma HLS INLINE
 	DF_LOOP0:for (int i = 0; i < DIM; ++i) {
+#pragma HLS UNROLL
 		if (input[i] < dropout_rate) {
 			output[i] = 0;
 		} else {
